a8dd5f78d6d62ffc5d699773e551ec09b0e9d68d
Mikael Antony <mikael11antony@gmail.com>
2025-01-11 16:56:40 -0300

    recomeçei o projeto e adicionei um menu hamburquer funcional

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 var pesq = document.getElementById('loadVideo');
+var menu = document.getElementById('menu')
+var mnstt = false
 
 pesq.setAttribute('onclick', 'pesqs()');
 
@@ -22,6 +24,39 @@ function extractVideoId(url) {
     var regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
 
     var match = url.match(regex);
-    
+
     return match ? match[1] : null;
+}
+
+menu.addEventListener('click', menut)
+
+function menut() {
+
+    let options = document.getElementById('options')
+
+    let line1 = document.getElementById('line1')
+    let line2 = document.getElementById('line2')
+    let line3 = document.getElementById('line3')
+
+    if (mnstt) {
+        line1.style.transform = ''
+
+        line2.style.width = '30px'
+        line2.style.opacity='1'
+
+        line3.style.transform = ''
+
+        options.style.transform = 'translate(150vw)'
+        mnstt = false
+    } else {
+        line1.style.transform = 'rotate(-45deg) translate(-7px, 5.2px)'
+
+        line2.style.width = '0px'
+        line2.style.opacity='0'
+
+        line3.style.transform = 'rotate(45deg) translate(-7px, -5.2px)'
+        
+        options.style.transform = 'translate(70vw)'
+        mnstt = true
+    }
 }
